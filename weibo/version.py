8f9b548a72ddb77cc292d538f4320a2f4a2fb8ef
zwei <leidong@kingsoft.com>
2016-06-11 00:08:56 +0800

    add cfg file

--- weibo/version.py
+++ weibo/version.py
@@ -1,6 +1,13 @@
 """ SAMO version info """
+from pbr import version as pbr_version
 
 OBJECT="weibo"
 OBJECT_CONF="weibo.conf"
 VERSION = 1.0
 INIT_VERSION = 0
+
+WEIBO_PACKAGE = None  # OS distro weibo version suffix
+
+loaded = False
+version_info = pbr_version.VersionInfo('weibo')
+version_string = version_info.version_string
